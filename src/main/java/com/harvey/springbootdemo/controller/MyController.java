24f8366343c057d3136a2729c2b6c5d089de3100
harveyli <harveyli@fortinet.com>
2022-12-31 19:29:43 -0800

    test

---
+++ src/main/java/com/harvey/springbootdemo/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.harvey.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class MyController {
+    @GetMapping("hello")
+    public String hello() {
+        return "hello world!";
+    }
+}
